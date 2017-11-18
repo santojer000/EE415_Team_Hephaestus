886442fdc74ea4fb79a206d00a9ed998a9bc7ffa
Adoney <arongalvan03@gmail.com>
2017-11-17 17:51:51 -0800

    GUI code

---
+++ SerialMonitor/SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.BaudRateComboBox = new System.Windows.Forms.ComboBox();
+            this.FrameSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ClearPlotButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SerialPlot = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SerialPlot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Frame Size";
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(33, 36);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(121, 24);
+            this.PortComboBox.TabIndex = 3;
+            // 
+            // BaudRateComboBox
+            // 
+            this.BaudRateComboBox.FormattingEnabled = true;
+            this.BaudRateComboBox.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.BaudRateComboBox.Location = new System.Drawing.Point(33, 94);
+            this.BaudRateComboBox.Name = "BaudRateComboBox";
+            this.BaudRateComboBox.Size = new System.Drawing.Size(121, 24);
+            this.BaudRateComboBox.TabIndex = 4;
+            // 
+            // FrameSizeComboBox
+            // 
+            this.FrameSizeComboBox.FormattingEnabled = true;
+            this.FrameSizeComboBox.Items.AddRange(new object[] {
+            "7",
+            "8",
+            "9"});
+            this.FrameSizeComboBox.Location = new System.Drawing.Point(33, 153);
+            this.FrameSizeComboBox.Name = "FrameSizeComboBox";
+            this.FrameSizeComboBox.Size = new System.Drawing.Size(121, 24);
+            this.FrameSizeComboBox.TabIndex = 5;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(33, 200);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(121, 23);
+            this.OpenButton.TabIndex = 6;
+            this.OpenButton.Text = "Open Port";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(33, 241);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(121, 23);
+            this.CloseButton.TabIndex = 7;
+            this.CloseButton.Text = "Close Port";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ClearPlotButton
+            // 
+            this.ClearPlotButton.Location = new System.Drawing.Point(34, 285);
+            this.ClearPlotButton.Name = "ClearPlotButton";
+            this.ClearPlotButton.Size = new System.Drawing.Size(120, 23);
+            this.ClearPlotButton.TabIndex = 11;
+            this.ClearPlotButton.Text = "Clear Plot";
+            this.ClearPlotButton.UseVisualStyleBackColor = true;
+            this.ClearPlotButton.Click += new System.EventHandler(this.ClearPlotButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGray;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ClearPlotButton);
+            this.panel2.Controls.Add(this.PortComboBox);
+            this.panel2.Controls.Add(this.CloseButton);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.OpenButton);
+            this.panel2.Controls.Add(this.BaudRateComboBox);
+            this.panel2.Controls.Add(this.FrameSizeComboBox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(195, 805);
+            this.panel2.TabIndex = 13;
+            // 
+            // SerialPlot
+            // 
+            this.SerialPlot.AllowDrop = true;
+            this.SerialPlot.BackColor = System.Drawing.Color.DimGray;
+            chartArea1.AxisX.Maximum = 1000D;
+            chartArea1.AxisX.Minimum = -1000D;
+            chartArea1.AxisX.Title = "Time (ms)";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            chartArea1.AxisY.Title = "G Force (mg)";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            chartArea1.BackColor = System.Drawing.Color.DarkGray;
+            chartArea1.Name = "Plot";
+            this.SerialPlot.ChartAreas.Add(chartArea1);
+            this.SerialPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.SerialPlot.Legends.Add(legend1);
+            this.SerialPlot.Location = new System.Drawing.Point(195, 0);
+            this.SerialPlot.Name = "SerialPlot";
+            this.SerialPlot.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "Plot";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "X_Data";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.ChartArea = "Plot";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Blue;
+            series2.Legend = "Legend1";
+            series2.Name = "Y_Data";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.ChartArea = "Plot";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series3.Legend = "Legend1";
+            series3.Name = "Z_Data";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.SerialPlot.Series.Add(series1);
+            this.SerialPlot.Series.Add(series2);
+            this.SerialPlot.Series.Add(series3);
+            this.SerialPlot.Size = new System.Drawing.Size(1296, 805);
+            this.SerialPlot.TabIndex = 10;
+            this.SerialPlot.Text = "Accelerometer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1491, 805);
+            this.Controls.Add(this.SerialPlot);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Serial Mointor";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SerialPlot)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.ComboBox BaudRateComboBox;
+        private System.Windows.Forms.ComboBox FrameSizeComboBox;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button ClearPlotButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart SerialPlot;
+    }
+}
+
---
+++ SerialMonitor/SerialMonitor/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialMonitor
+{
+    public partial class Form1 : Form
+    {
+        SerialPort myport = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            getavaliableports();
+            CloseButton.Enabled = false;
+            ClearPlotButton.Enabled = false;
+            SerialPlot.Anchor = AnchorStyles.Bottom;
+            SerialPlot.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+        }
+        private void getavaliableports()
+        {
+            PortComboBox.Items.AddRange(SerialPort.GetPortNames());
+        }
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+          
+                myport.BaudRate = Convert.ToInt32(BaudRateComboBox.Text);
+                myport.PortName = PortComboBox.Text;
+                myport.DataBits = Convert.ToInt32(FrameSizeComboBox.Text);
+                myport.Parity = Parity.None;
+                myport.StopBits = StopBits.One;
+                myport.Open();
+
+                CloseButton.Enabled = true;
+                OpenButton.Enabled = false;
+                ClearPlotButton.Enabled = true;
+                timer1.Enabled = true;
+           
+        }
+
+        private void updateplot()
+        {
+            byte[] temp_data = new byte[4];
+            int[] x_data_set = new int[1024];
+            float[] x_data_set_float = new float[1024];
+
+            SerialPlot.Invoke(new Action(() =>
+            {
+                foreach (var series in SerialPlot.Series)
+                {
+                    series.Points.Clear();
+                }
+
+            }));
+
+            for (int i = 0; i < 1024; i++)
+            {
+               for(int j = 0; j < 4; j++)
+                {
+                    temp_data[j] = (byte)myport.ReadByte();
+                }
+                x_data_set_float[i] = (float)(int)((temp_data[0] << 24) | (temp_data[1] << 16) | (temp_data[2] << 8) | (temp_data[3]));
+                x_data_set_float[i] = x_data_set_float[i] / 100000;
+            }
+            float x = -1000;
+            float inc = 2000 / 1024;
+            for(int k = 0 ; k < 1024; k++)
+            {
+
+                SerialPlot.Invoke(new Action(() =>
+                {
+                    SerialPlot.Series["X_Data"].Points.AddXY(x, x_data_set_float[k]);
+
+                }));
+                x = x + (inc);
+            }
+            x = -1000;
+
+        }
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            myport.Close();
+            BaudRateComboBox.ResetText();
+            FrameSizeComboBox.ResetText();
+            PortComboBox.ResetText();
+            OpenButton.Enabled = true;
+            CloseButton.Enabled = false;
+            ClearPlotButton.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        private void ClearPlotButton_Click(object sender, EventArgs e)
+        {
+            ClearPlot();
+        }
+        private void ClearPlot()
+        {
+            SerialPlot.Invoke(new Action(() =>
+            {
+                foreach (var series in SerialPlot.Series)
+                {
+                    series.Points.Clear();
+                }
+
+            }));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            updateplot();
+            timer1.Enabled = true;
+        }
+    }
+}
